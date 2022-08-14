b0543441bb817ca26511a8ab99acf3629cc11e80
MohdSajid12 <108975916+MohdSajid12@users.noreply.github.com>
2022-08-15 00:36:03 +0530

    Add files via upload

---
+++ Auto-Text-Writer/script.js
@@ -0,0 +1,17 @@
+const text ='I am The Best 😀'
+
+
+let index = 0;
+
+function writeText() {
+    
+    document.body.innerText = text.slice(0,index);
+
+    index++;
+
+    if(index > text.length){
+        index =0;
+    }
+}
+
+setInterval(writeText , 300);
